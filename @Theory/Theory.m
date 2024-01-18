b3280f4eef374719cd481ee1449ee5c1d83ce718
ncotti <ncotti@frba.utn.edu.ar>
2024-01-17 21:59:28 -0300

    Symbol Error Rate

--- @Theory/Theory.m
+++ @Theory/Theory.m
@@ -13,8 +13,14 @@ classdef Theory
     end
 
     methods(Static)
+        % Channel capacity
         C_max = maximum_channel_capacity(EsNo_dB)
         C_pdf = channel_capacity_from_pdf(u_r, constellation, h_c, EsNo_dB)
+
+        % Symbol error rate
+        ser = ser_ricean(mod_type, M, EsNo_dB, PlosPnlos_dB)
+        ser = ser_rayleigh(mod_type, M, EsNo_dB)
+        ser = ser_AWGN(mod_type, M, EsNo_dB)
     end
 end
 
--- symbol_error_rate.m
+++ symbol_error_rate.m
@@ -1,53 +1,76 @@
 %% Symbol Error Rate.
-% Calculate the SER for different modulation types for different channel
-% types.
+% Calculate the SER for different modulation types and channels, and 
+% compare with theoretical values.
 
 clc; clear; close all;
-addpath("Functions");
 
 %% Parameters
-sample_qtty = 1e5;                  % Number of samples
-EsNo_dB = 0:2:40;                   % EsNo
-mod_type = ModulationTypes.QAM;     % Modulation type.
-M = [4,16,64];                      % Number of symbols
-channel_type = ChannelTypes.Ricean;   % Channel type
+sample_qtty = 1e5;                  % Number of samples.
+EsNo_dB = 0:2:40;                   % EsNo.
+mod_type = "QAM";                   % Modulation type.
+M = [4,16,64];                      % Number of symbols.
+channel_type = ChannelTypes.AWGN;   % Channel type.
+PlosPnlos_dB = 20;                  % Ratio between power of LOS and NLOS.
+L = 10;                             % Oversampling factor
+beta = 0.8;                         % Roll-off SRRC
 
-% Ratio between power of LOS and NLOS components. Used for Ricean channel
-PlosPnlos_dB = 20; 
+% Taps for FIR filters. Note: The bigger this value, the closer the 
+% theoretical and simulated SERs are.
+nTaps = 50;
 
 %% Calculations
+colors = ["b", "r", "g", "c", "m", "k"];    % Colors for plotting
+legendString = cell(1, 2*length(M));        % For legend in plot, as "16-QAM"
+
+ser = zeros(1, length(EsNo_dB));            % Symbol error rate
+
 for m=1:1:length(M)
-    x = randi([0, M(m)-1], 1, sample_qtty);
-    ser = zeros(1, length(EsNo_dB));
+    d = randi([0, M(m)-1], 1, sample_qtty); % Input symbols
+
+    % Modulator
+    [u, constellation] = Modulator.modulate(d, mod_type, M(m));
+    v = Modulator.upsample(u, L);
+    [s, ~, delay_tx] = Modulator.pulse_shaping_srrc(v, beta, L, nTaps);
 
     for i=1:1:length(EsNo_dB)
-        mod = Modulator(mod_type, M(m));
-        [s, constellation] = mod.modulate(x);
-    
-        channel = Channel(channel_type, EsNo_dB(i), PlosPnlos_dB=PlosPnlos_dB);
-        [r, n, h] = channel.add_noise(s);
+        % Channel
+        switch channel_type
+            case ChannelTypes.AWGN
+                [r, h_c] = Channel.add_awgn_noise(s, EsNo_dB(i), L);
+            case ChannelTypes.Rayleigh
+                [r, h_c] = Channel.add_rayleigh_noise(s, EsNo_dB(i), L);
+            case ChannelTypes.Ricean
+                [r, h_c] = Channel.add_ricean_noise(s, EsNo_dB(i), L, PlosPnlos_dB);
+        end
+        
+        % Demodulator
+        r = Demodulator.flat_fading_equalizer(r, h_c);
+        [v_r, ~, delay_rx] = Demodulator.pulse_filter_srrc(r, beta, L, nTaps);
+        u_r = Demodulator.downsample(v_r, L, delay_tx + delay_rx);
+        d_r = Demodulator.demodulate(u_r, mod_type, M(m), constellation);
 
-        demod = Demodulator(mod_type, M(m), constellation);
-        y = demod.demodulate(r);
-
-        ser(i) = sum(x~=y)/sample_qtty;
+        ser(i) = sum(d~=d_r)/sample_qtty;
     end
 
-    if (channel_type == ChannelTypes.AWGN)
-        ser_theory = ser_AWGN(mod_type, M(m), EsNo_dB);
-    elseif (channel_type == ChannelTypes.Rayleigh)
-        ser_theory = ser_rayleigh(mod_type, M(m), EsNo_dB);
-    elseif (channel_type == ChannelTypes.Ricean)
-        ser_theory = ser_ricean(mod_type, M(m), EsNo_dB, PlosPnlos_dB);
+    switch channel_type
+        case ChannelTypes.AWGN
+            ser_theory = Theory.ser_AWGN(mod_type, M(m), EsNo_dB);
+        case ChannelTypes.Rayleigh
+            ser_theory = Theory.ser_rayleigh(mod_type, M(m), EsNo_dB);
+        case ChannelTypes.Ricean
+            ser_theory = Theory.ser_ricean(mod_type, M(m), EsNo_dB, PlosPnlos_dB);
     end
 
-    semilogy(EsNo_dB, ser); hold on;
-    semilogy(EsNo_dB, ser_theory, LineStyle="--"); hold on;
+    semilogy(EsNo_dB, ser, Color=colors(m), LineStyle="-"); hold on;
+    semilogy(EsNo_dB, ser_theory, Color=colors(m), LineStyle="--"); hold on;
+
+    legendString{2*m-1} = strcat(num2str(M(m)), "-", mod_type, " Simulated");
+    legendString{2*m}   = strcat(num2str(M(m)), "-", mod_type, " Theoretical");
 end
 
 grid on;
+legend(legendString);
 xlabel("Es/N0 [dB]");
 ylabel("SER");
 title("Symbol Error Rate");
-ylim([1e-6, 1])
-
+ylim([1e-6, 1]);
